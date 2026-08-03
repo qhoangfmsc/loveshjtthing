9b6c3da1b2c4cc763b2eb5c6b4e2524dbd1f74a5
Hoàng Nguyễn <87424201+qhoangfmsc@users.noreply.github.com>
2026-08-03 10:05:42 +0700

    feat: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,9 +42,7 @@ const brotherSignature = localFont({
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL
   ? process.env.NEXT_PUBLIC_SITE_URL
-  : process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000";
+  : "http://localhost:3000";
 
 const title = "💖 Văn Kiều Bảo Trân 💖";
 const description =
